4dd9b16026e44b2c3dd226bb204f875280676506
oma1s <omais.rasool.khan@gmail.com>
2025-11-12 18:41:30 -0500

    checkout interactive

--- data/products.js
+++ data/products.js
@@ -1,3 +1,15 @@
+export function getProduct(productId){
+  let product;
+    for(let i = 0; i < products.length; i++){
+      if(products[i].id === productId){
+          product = products[i];
+          break;
+      }
+    }
+  return product;
+}
+
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
---
+++ scripts/checkout/paymentSummary.js
@@ -0,0 +1,62 @@
+import {cart, getCartQuantity} from '../../data/cart.js';
+import { getProduct } from '../../data/products.js';
+import {getDeliveryOption} from '../../data/deliveryOptions.js'
+import { formatCurrency } from '../utils/money.js';
+
+
+export function renderPaymentSummsary(){
+    let productPriceCents = 0;
+    let shippingPriceCents = 0;
+    cart.forEach(cartItem => {
+        const product = getProduct(cartItem.productId);
+        productPriceCents += product.priceCents * cartItem.quantity;
+
+        const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
+        shippingPriceCents += deliveryOption.priceCents;
+
+    });
+    const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
+    const totalTax = totalBeforeTaxCents * 0.1;
+
+    const totalCents = totalBeforeTaxCents + totalTax;
+
+    const cartQuantity = getCartQuantity();
+    const str = cartQuantity <= 1 ? 'Item' : 'Items';
+
+    const paymentSummaryHTML = `
+          <div class="payment-summary-title">
+            Order Summary
+          </div>
+
+          <div class="payment-summary-row">
+            <div class="js-payment-cart-quantity">${str} (${cartQuantity}):</div>
+            <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Shipping &amp; handling:</div>
+            <div class="payment-summary-money">$${formatCurrency(shippingPriceCents)}</div>
+          </div>
+
+          <div class="payment-summary-row subtotal-row">
+            <div>Total before tax:</div>
+            <div class="payment-summary-money">$${formatCurrency(totalBeforeTaxCents)}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Estimated tax (10%):</div>
+            <div class="payment-summary-money">$${formatCurrency(totalTax)}</div>
+          </div>
+
+          <div class="payment-summary-row total-row">
+            <div>Order total:</div>
+            <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
+          </div>
+
+          <button class="place-order-button button-primary">
+            Place your order
+          </button>
+    `;
+
+    document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;   
+};
