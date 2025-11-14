0c70b41b84d03e9cd79acb1f88b4102f5e9c31df
oma1s <omais.rasool.khan@gmail.com>
2025-11-14 15:47:36 -0500

    Using async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,11 +2,18 @@ import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummsary } from './checkout/paymentSummary.js';
 import { loadProducts, loadProductsFetch } from '../data/products.js';
 
+async function loadPage(){
+    await loadProductsFetch();
+    renderOrderSummary();
+    renderPaymentSummsary();
+};
+loadPage();
+/*
 loadProductsFetch().then(() =>{
     renderOrderSummary();
     renderPaymentSummsary();
 });
-
+*/
 /*
 new Promise((resolve) =>{
     loadProducts(() =>{
